7b02497a96956fe225ce3d710ce88a85e4d5d9a5
2024211369-zhuzhuoyan <2024211369@stu.hit.edu.cn>
2026-05-16 02:45:08 +0800

    i think it is final

--- minirvEMU/minirvEMU.c
+++ minirvEMU/minirvEMU.c
@@ -21,9 +21,10 @@ void w4(uint32_t addr,uint32_t val)
  M[addr]     = val & 0xFF;
     M[addr + 1] = (val >> 8) & 0xFF;
     M[addr + 2] = (val >> 16) & 0xFF;
-    M[addr + 3] = (val >> 24) & 0xFF;}
-
-uint32_t inst=M[pc];
+    M[addr + 3] = (val >> 24) & 0xFF;}                                                                                                                                           
+int main(){
+  
+while(1){uint32_t inst=M[pc];
   int op=inst&0x7f;
   uint32_t rd=(inst>>7)&0x1f;
   uint32_t fun3=(inst>>12)&0x7;
@@ -32,53 +33,66 @@ uint32_t inst=M[pc];
  uint32_t funct7=(inst>>25)&0x7f;
   int32_t immi= (int32_t)inst >> 20;
 	int32_t imms=((inst>>25)<<5)|((inst>>7)&0x1f);
-if (imms & 0x800) imms |= 0xfffff000;    
-uint32_t immu=inst & 0xfffff000;                                                                                                                                            
-int main(){
+if (imms & 0x800); imms |= 0xfffff000;    
+uint32_t immu=inst & 0xfffff000;
 
-while(1){
-	uint32_t inst=M[pc];
 	pc=pc+4;
-	switch(op){
+	switch(op){u
+	/*   ADD  */	
 case 0x33:
     {if (fun3 == 0x0 && funct7 == 0x00) {
-        x[rd] = x[rs1] + x[rs2];
+        x[rd]=x[rs1] + x[rs2];
     }
     break;}
-
-
-
-
+/*  ADDI */
 case 0x13:
    { if (fun3 == 0x0) {
-        x[rd] = x[rs1] + immi;
+        x[rd]=x[rs1] + immi;
     }
     break;}
 
 
-
+/* lui */
 case 0x37:
-   { x[rd] = immu;
+   { x[rd]=immu;
     break;}
-
+/* lw  lbu*/
 		case 0x03:
    { if (fun3 == 0x2) {
-        uint32_t addr = x[rs1] + immi;
+        uint32_t addr=x[rs1] + immi;
         x[rd] = r4(addr);
-    }else if (funct3 == 0x4)
-                x[rd] = r8(x[rs1] + imm_i);
+    }else if (fun3 == 0x4)
+             uint32_t addr=x[rs1] + immi;
+            uint8_t x=r(addr);
+						x[rd]=uint32_t x;
     break;}
 
 
-
+/*  sw  sb*/
 		case 0x23:
    { if (fun3 == 0x2) {
-        uint32_t addr = x[rs1] + imm_s;
+        uint32_t addr=x[rs1] + imms;
         w4(addr, x[rs2]);
-    }else if (funct3 == 0x0)
-                w8(x[rs1] + imm_s, x[rs2]);
+    }else if (fun3 == 0x0)
+            uint32_t addr=x[rs1] + imms;
+						uint8_t va1l=x[rs2]&0xff;
+						w(addr,va1l)
     break;
-	 }
+	 } 
+	 /* jalr*/ 
+	 case:0x67
+	 {
+if(fun3==0x0)
+{
+ uint32_t target = x[rs1] + immi;
+
+    x[rd] = pc + 4;   
+    pc = target & ~1;   
+
+    break;
+}
+
+
 
 
 
