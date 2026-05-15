7ea791d10301ff48c909929dd788c0f2b8542bd1
2024211369-zhuzhuoyan <2024211369@stu.hit.edu.cn>
2026-05-16 07:33:54 +0800

    增加了显示功能

--- minirvEMU/build/native/minirvEMU.d
+++ minirvEMU/build/native/minirvEMU.d
@@ -2,5 +2,4 @@
  /home/zzy/ysyx/minirvEMU/minirvEMU.c \
  /home/zzy/ysyx-workbench/abstract-machine/am/include/am.h \
  /home/zzy/ysyx-workbench/abstract-machine/am/include/arch/native.h \
- /home/zzy/ysyx-workbench/abstract-machine/am/include/amdev.h \
- /home/zzy/ysyx-workbench/abstract-machine/klib/include/klib-macros.h
+ /home/zzy/ysyx-workbench/abstract-machine/am/include/amdev.h
